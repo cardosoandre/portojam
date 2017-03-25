52e8ffd73e78c679787e1c8f8736737e89d38b91
mcb3 <mcb3@cin.ufpe.br>
2017-03-25 15:24:34 -0300

    Structuring internal input management to support multiplayer.

---
+++ Keyster/Assets/keyboardMap.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct input
+{
+	KeyCode key;
+	int playerID;
+}
+
+public class keyboardMap : MonoBehaviour {
+
+	public List<KeyCode> pressedKeys;
+
+	// Use this for initialization
+	void Start () {
+		pressedKeys = new List<KeyCode>();
+	}
+
+	List<KeyCode> getPressedKeys()
+	{
+		var buffer = pressedKeys;
+		pressedKeys.Clear ();
+
+		return buffer;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			if(!pressedKeys.Contains(KeyCode.Alpha1))
+			{
+				pressedKeys.Add(KeyCode.Alpha1);
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			if(!pressedKeys.Contains(KeyCode.Alpha2))
+			{
+				pressedKeys.Add(KeyCode.Alpha2);
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			if(!pressedKeys.Contains(KeyCode.Alpha3))
+			{
+				pressedKeys.Add(KeyCode.Alpha3);
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha4))
+		{
+			if(!pressedKeys.Contains(KeyCode.Alpha4))
+			{
+				pressedKeys.Add(KeyCode.Alpha4);
+			}
+		}
+	}
+}
