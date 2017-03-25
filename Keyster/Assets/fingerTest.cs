de1e0224d7c1b20daeb47157c5875b5a1ebd0352
André Cardoso <amc.acardoso@gmail.com>
2017-03-25 11:45:40 -0300

    First Commit

---
+++ Keyster/Assets/fingerTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fingerTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+			print ("apertando A");
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+			print ("apertando S");
+		}
+
+		if (Input.GetKeyDown (KeyCode.D)) {
+			print ("apertando D");
+		}
+
+		if (Input.GetKeyDown (KeyCode.F)) {
+			print ("apertando F");
+		}
+
+		if (Input.GetKeyDown (KeyCode.G)) {
+			print ("apertando G");
+		}
+
+		if (Input.GetKeyDown (KeyCode.H)) {
+			print ("apertando H");
+		}
+
+		if (Input.GetKeyDown (KeyCode.J)) {
+			print ("apertando J");
+		}
+
+		if (Input.GetKeyDown (KeyCode.K)) {
+			print ("apertando K");
+		}
+
+		if (Input.GetKeyDown (KeyCode.L)) {
+			print ("apertando L");
+		}
+		
+	}
+}
