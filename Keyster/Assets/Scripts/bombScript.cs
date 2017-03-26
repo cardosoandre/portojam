f773e9fc077863c510976d31635480f779655f44
mcb3 <mcb3@cin.ufpe.br>
2017-03-26 04:31:45 -0300

    bomb blinking

--- Keyster/Assets/Scripts/bombScript.cs
+++ Keyster/Assets/Scripts/bombScript.cs
@@ -8,24 +8,51 @@ public class bombScript : MonoBehaviour {
 	public GameObject explosion;
 
 	private bool grounded;
-	private float time;
+	private float timer;
+	private Color originalColor;
 
 	// Use this for initialization
 	void Start () {
-		time = 10;
+		timer = 15;
 		shouldExplode = false;
+		originalColor = gameObject.GetComponent<MeshRenderer> ().material.color;
+		Invoke ("Blink", 1f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (0 < time)
+		if (0 < timer)
 		{
-			time = time - Time.deltaTime;
+			timer = timer - Time.deltaTime;
 		} else 
 		{
 			shouldExplode = true;
 		}	
+	}
+
+	void Blink()
+	{
+		gameObject.GetComponent<MeshRenderer> ().material.color = Color.white;
+		Invoke ("BlinkBack", 0.1f);
+
+		if (8 < timer) 
+		{
+			Invoke ("Blink", 1f);
+		}
+		else if (3 < timer)
+		{
+			Invoke ("Blink", 0.5f);
+		}
+		else
+		{
+			Invoke ("Blink", 0.25f);
+		}
+	}
+
+	void BlinkBack()
+	{
+		gameObject.GetComponent<MeshRenderer> ().material.color = originalColor;
 	}
 
 	void OnCollisionEnter(Collision other)
@@ -38,6 +65,5 @@ public class bombScript : MonoBehaviour {
 				Destroy (gameObject);
 			}
 		}
-
 	}
 }
